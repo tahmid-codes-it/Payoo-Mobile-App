e42cce22a91742b5309de77d5473316f0d062aa6
tahmid-codes-it <mdtahmidiiuc@gmail.com>
2025-08-24 12:38:18 +0600

    completed home page

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 document.getElementById("login-btn").addEventListener("click", function (e){
     e.preventDefault();
-    console.log("Login Button Clicked");
+
+    const mobileNumber = 0123456789;
+    const pinNumber = 1111;
+
+    const userMobileNumber = document.getElementById("mobile-number").value;
+
+    const userMobileNumberConverted = parseInt(userMobileNumber);
+
+    const userPinNumber = document.getElementById("pin-number").value;
+
+    const userPinNumberConverted = parseInt(userPinNumber);
+
+    if (mobileNumber === userMobileNumberConverted && pinNumber === userPinNumberConverted){
+        window.location.href = "home.html";
+    }
+
+    else{
+        alert("invalid credentials")
+    }
 })
